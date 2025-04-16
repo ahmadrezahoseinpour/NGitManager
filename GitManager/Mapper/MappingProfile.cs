4f7c6fa900402c703065ec8472f7bb767285c620
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-04-16 17:53:40 +0330

    Add Mapping

---
+++ GitManager/Mapper/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GitManager.Dto.Epic;
+using GitManager.Dto.Issue;
+using GitManager.Dto.User;
+using NGitLab.Models;
+
+namespace GitManager.Mapper
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<Identity, IdentityDto>();
+
+            CreateMap<Issue, IssueDto>();
+            CreateMap<Author, AuthorDto>();
+            CreateMap<Assignee, AssigneeDto>();
+            CreateMap<IssueEpic, IssueEpicDto>();
+            CreateMap<Milestone, MilestoneDto>();
+            CreateMap<References, ReferencesDto>();
+
+            CreateMap<Epic, EpicDto>();
+        }
+    }
+}
