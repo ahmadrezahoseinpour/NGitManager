1abfbdf6bd97cdf3babdd654299c16178339c9f5
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-04-28 17:27:17 +0330

    Update dto

--- GitManager/Dto/Epic/EpicDto.cs
+++ GitManager/Dto/Epic/EpicDto.cs
@@ -8,14 +8,11 @@ namespace GitManager.Dto.Epic
 {
     public class EpicDto
     {
-        [JsonPropertyName("id")]
-        public long Id { get; set; }
-
-        [JsonPropertyName("iid")]
-        public long EpicIid { get; set; }
-
         [JsonPropertyName("group_id")]
         public long GroupId { get; set; }
+
+        [JsonPropertyName("epic_iid")]
+        public long EpicId { get; set; }
 
         [JsonPropertyName("title")]
         public string Title { get; set; }
@@ -24,18 +21,9 @@ namespace GitManager.Dto.Epic
         public string Description { get; set; }
 
         [JsonPropertyName("labels")]
-        public string[] Labels { get; set; }
+        public string Labels { get; set; }
 
-        [JsonPropertyName("state")]
-        public EpicState State { get; set; }
-
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        [JsonPropertyName("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-
-        [JsonPropertyName("web_url")]
-        public string WebUrl { get; set; }
+        [JsonPropertyName("state_event")]
+        public string State { get; set; }
     }
 }
--- GitManager/Dto/Issue/IssueDto.cs
+++ GitManager/Dto/Issue/IssueDto.cs
@@ -10,84 +10,51 @@ namespace GitManager.Dto.Issue
 {
     public class IssueDto
     {
-        [JsonPropertyName("id")]
-        public long Id { get; set; }
-
-        [JsonPropertyName("iid")]
-        public long IssueId { get; set; }
-
-        [JsonPropertyName("project_id")]
-        [Required(ErrorMessage ="شناسه پروژه اجباری است")]
-        [Range(1,int.MaxValue)]
+        [JsonIgnore]
         public long ProjectId { get; set; }
 
+        [JsonPropertyName("issue_id")]
+        public long IssueId { get; set; }
+
+        [Required]
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
         [JsonPropertyName("description")]
         public string Description { get; set; }
 
+        [JsonPropertyName("assignee_id")]
+        public long? AssigneeId { get; set; }
+
+        [JsonPropertyName("assignee_ids")]
+        public long[] AssigneeIds { get; set; }
+
+        [JsonPropertyName("milestone_id")]
+        public long? MileStoneId { get; set; }
+
         [JsonPropertyName("labels")]
-        public string[] Labels { get; set; }
+        public string Labels { get; set; }
 
-        [JsonPropertyName("milestone")]
-        public MilestoneDto Milestone { get; set; }
+        [JsonPropertyName("confidential")]
+        public bool Confidential { get; set; }
 
-        [JsonPropertyName("assignee")]
-        public AssigneeDto Assignee { get; set; }
+        [JsonPropertyName("due_date")]
+        public DateTime? DueDate { get; set; }
 
-        [JsonPropertyName("assignees")]
-        public AssigneeDto[] Assignees { get; set; }
-
-        [JsonPropertyName("author")]
-        public AuthorDto Author { get; set; }
-
-        [JsonPropertyName("state")]
-        public string State { get; set; }
-
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        [JsonPropertyName("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-
-        [JsonPropertyName("closed_at")]
-        public DateTime ClosedAt { get; set; }
-
-        [JsonPropertyName("closed_by")]
-        public UserDto ClosedBy { get; set; }
-
-        //[JsonPropertyName("due_date")]
-        //public DateTime? DueDate { get; set; }
-
-        //[JsonPropertyName("web_url")]
-        //public string WebUrl { get; set; }
-
-        //[JsonPropertyName("merge_requests_count")]
-        //public int MergeRequestsCount { get; set; }
-
-        [JsonPropertyName("epic")]
-        public IssueEpicDto Epic { get; set; }
-
-        //[JsonPropertyName("confidential")]
-        //public bool Confidential { get; set; }
+        [JsonPropertyName("epic_id")]
+        public long? EpicId { get; set; }
 
         [JsonPropertyName("weight")]
         public int? Weight { get; set; }
 
-        //[JsonPropertyName("issue_type")]
-        //public string IssueType { get; set; }
+        [JsonPropertyName("state_event")]
+        public string State { get; set; }
 
-        //[JsonPropertyName("moved_to_id")]
-        //public long? MovedToId { get; set; }
+        [JsonPropertyName("discussion_locked")]
+        public bool? DiscussionLocked { get; set; }
 
-        //[JsonPropertyName("references")]
-        //public ReferencesDto References { get; set; }
+        [JsonPropertyName("author_id")]
+        public long AuthorId { get; set; }
 
-        //[JsonPropertyName("user_notes_count")]
-        //public int UserNotesCount { get; set; }
-
-        //[JsonPropertyName("discussion_locked")]
-        //public bool DiscussionLocked { get; set; }
     }
 }
