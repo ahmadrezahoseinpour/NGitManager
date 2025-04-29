130e4aca77860727281567be5b29545edb17fcaa
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-04-29 18:47:51 +0330

    Update pages

--- GitUI/Components/Pages/Issues.razor.cs
+++ GitUI/Components/Pages/Issues.razor.cs
@@ -1,34 +1,43 @@
 ﻿using GitManager.Dto.Issue;
+using GitManager.Dto.User;
 using GitManager.Interface;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using NGitLab.Models;
+using System.Linq.Expressions;
 
 namespace GitUI.Components.Pages
 {
     public partial class Issues
     {
         #region Properties
+
+        [Inject]
+        public ICommonService CommonService { get; set; }
+        [Inject]
+        public IIssueService IssueService { get; set; }
         [Parameter] public int ProjectId { get; set; }
         [Parameter] public int IssueIid { get; set; }
         public string ErrorMessage { get; set; }
         private string SelectedState { get; set; } = "All";
-        public bool IsLoading { get; set; }
-        [Inject]
-        public ILabelService LabelService { get; set; }
-        [Inject]
-        public IIssueService IssueService { get; set; }
-
         private string Title { get; set; }
         private string Description { get; set; }
 
         private string ModalDisplay { get; set; } = "none;";
         private bool _editDialog { get; set; }
         private string[] Labels { get; set; }
-        private IssueDto Issue { get; set; } = new();
+        private string[] Milestones { get; set; }
+        public IssueDto Issue { get; set; } = new();
         public IssueDto UpdateDto { get; set; } = new();
         public IssueDto AddDto { get; set; } = new();
-        public List<IssueDto> IssuesList { get; set; }= [];
+        public MilestoneDto Milestone { get; set; }
+        public List<IssueDto> IssuesList { get; set; } = [];
         private string SelectedLabels { get; set; }
+        private string SelectedMilestone { get; set; } = "";
+
+        private bool IsLoading = false; 
+        private bool HasSearched = false;
+
 
         protected override async Task OnInitializedAsync()
         {
@@ -43,32 +52,39 @@ namespace GitUI.Components.Pages
             ////for Get
             //Issue = await IssueService.Get(ProjectId, IssueIid);
             Issue.ProjectId = 0;
+            Issue.Milestone = new MilestoneDto();
+            Issue.Assignee = new AssigneeDto();
+            
+            Issue.Author= new AuthorDto();
+            Issue.ClosedBy = new UserDto();
+            Issue.Epic = new IssueEpicDto();
+            Issue.References = new ReferencesDto();
+
             StateHasChanged();
-            base.OnInitialized();
+            await base.OnInitializedAsync();
         }
         #endregion
 
         #region Modals
 
 
-        private void ShowEditDialog()
+        private void ShowEditDialog(IssueDto dto)
         {
+
+            SelectedLabels = string.Join(",", dto.Labels);
             ModalDisplay = "flex;";
             _editDialog = true;
-            //ModalClass = "show";
-            //ShowBackdrop = true;
             StateHasChanged();
         }
         private void CloseEditDialog()
         {
             ModalDisplay = "none;";
             _editDialog = false;
-            //ModalClass = "";
-            //ShowBackdrop = false;
             StateHasChanged();
         }
         private async Task SubmitEditDialog()
         {
+            Issue.Milestone.Title = SelectedMilestone;
             await UpdateIssue();
         }
         #endregion
@@ -76,58 +92,134 @@ namespace GitUI.Components.Pages
         #region Mothods
         private async Task<List<IssueDto>> GetAllIssue(int ProjectId)
         {
-            IssuesList = await IssueService.GetAll(ProjectId);
-            StateHasChanged();
-            return IssuesList;
+            try
+            {
+                IsLoading = true;
+                HasSearched = true;
+
+                IssuesList = await IssueService.GetAll(ProjectId);
+                StateHasChanged();
+                return IssuesList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+                return new List<IssueDto>();
+            }
+            finally
+            {
+
+                IsLoading = false;
+            }
         }
 
         private async Task CloseIssue()
         {
-            await IssueService.Close(UpdateDto);
-            await GetAllIssue(ProjectId);
-            StateHasChanged();
+            try
+            {
+                await IssueService.Close(UpdateDto);
+                await GetAllIssue(ProjectId);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
         }
 
         private async Task OpenIssue()
         {
-            await IssueService.Open(UpdateDto);
-            await GetAllIssue(ProjectId); 
-            StateHasChanged();
-        }
-        private void OnSelectionChanged(ChangeEventArgs e)
-        {
-            // For multi-select, e.Value is a string array of the selected values
-            if (e.Value is string selectedValues)
+            try
             {
-                UpdateDto.Labels = string.Join(",", selectedValues);
+                await IssueService.Open(UpdateDto);
+                await GetAllIssue(ProjectId);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
             }
         }
-        private async Task SearchIssues()
+        //private void OnMilestoneSelectChanged(ChangeEventArgs e)
+        //{
+
+        //    try
+        //    {
+        //        if (e.Value is string selectedValues)
+        //        {
+        //            UpdateDto.Milestone = UpdateDto.Milestone.Append(selectedValues).ToArray();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Error fetching issues: {ex.Message}");
+        //    }
+        //}
+        private void OnLabelSelectChanged(ChangeEventArgs e)
         {
-            await LoadIssues();
-            await GetAllLabels();
+            try
+            {
+                if (e.Value is string selectedValues)
+                {
+                    UpdateDto.Labels = UpdateDto.Labels.Append(selectedValues).ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
         }
-        private async Task GetAllLabels()
+
+        private void OnAssigneeSelectChanged(ChangeEventArgs e)
         {
-            Labels = [];
-            Labels = await LabelService.GetAllByProject(ProjectId);
+            try
+            {
+                if (e.Value is string selectedValues)
+                {
+                    //UpdateDto.Assignees = UpdateDto.Assignees.Append(selectedValues).ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
         }
+
         private async Task CreateIssue()
         {
-            var newIssue = await IssueService.Create(AddDto);
-            AddDto = new();
-            //NavigationManager.NavigateTo($"/issues/{ProjectId}/{newIssue.IssueId}");
+            try
+            {
+                var newIssue = await IssueService.Create(AddDto);
+                AddDto = new();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
         }
 
         private async Task UpdateIssue()
         {
-            var updatedIssue = await IssueService.Update(UpdateDto);
-            UpdateDto = new();
+            try
+            {
+                var updatedIssue = await IssueService.Update(UpdateDto);
+                UpdateDto = new();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
+        }
 
-            //NavigationManager.NavigateTo($"/issues/{ProjectId}/{updatedIssue.IssueId}");
+        private async Task SearchIssues()
+        {
+            await LoadIssues();
+            await GetAllMilestone();
+            await GetAllLabels();
+            HasSearched = true;
         }
         private async Task LoadIssues()
-        {   
+        {
             ProjectId = (int)Issue.ProjectId;
             if (ProjectId <= 0)
             {
@@ -136,7 +228,6 @@ namespace GitUI.Components.Pages
                 IsLoading = false;
                 return;
             }
-
             IsLoading = true;
             ErrorMessage = string.Empty;
             try
@@ -147,6 +238,7 @@ namespace GitUI.Components.Pages
                 IssuesList = SelectedState == "All"
                     ? allIssues
                     : allIssues.Where(i => i.State.ToLower() == SelectedState.ToLower()).ToList();
+
             }
             catch (Exception ex)
             {
@@ -157,6 +249,44 @@ namespace GitUI.Components.Pages
             finally
             {
                 IsLoading = false;
+            }
+        }
+        #endregion
+
+        #region Common Services
+
+        private async Task GetAllLabels()
+        {
+            try
+            {
+                Labels = [];
+                Labels = await CommonService.GetLabelsByProject(ProjectId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
+            }
+        }
+        private async Task GetAllMilestone()
+        {
+            try
+            {
+                Milestones = [];
+                //---------------------------------\\
+                //HARD CODE FOR MODERNIZATION GROUP\\
+                //---------------------------------\\
+                //---------------------------------\\
+
+                var milestoneTitles = new List<string>();
+                var res =  await CommonService.GetMilestonesByGroup(27);
+                foreach (var item in res) {
+                    milestoneTitles.Add(item.Title);
+                }
+                Milestones = milestoneTitles.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching issues: {ex.Message}");
             }
         }
         #endregion
