93245d2746c787afadfb63499c1cd7f77608bea5
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-04 18:45:20 +0330

    Update mapper

--- GitManager/Mapper/MappingProfile.cs
+++ GitManager/Mapper/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using GitManager.Dto.Epic;
 using GitManager.Dto.Issue;
+using GitManager.Dto.Issue.enums;
 using GitManager.Dto.User;
 using NGitLab;
 using NGitLab.Models;
@@ -21,9 +22,21 @@ namespace GitManager.Mapper
             CreateMap<IssueEpic, IssueEpicDto>();
             CreateMap<Milestone, MilestoneDto>();
             CreateMap<References, ReferencesDto>();
+            CreateMap<IssueQuery, IssueQueryDto>();
+            CreateMap<IssueState, IssueStateDto>();
+            CreateMap<IssueType, IssueTypeDto>();
+            CreateMap<QueryAssigneeId, QueryAssigneeIdDto>();
 
             CreateMap<Epic, EpicDto>();
             CreateMap<EpicQuery, EpicQueryDto>();
+            CreateMap<EpicState, EpicStateDto>();
+
+            // Mappings for the first Map call
+            CreateMap<IssueStateDto, IssueState>(); // Reverse map needed?
+            CreateMap<IssueTypeDto, IssueType>();   // Reverse map needed?
+            CreateMap<QueryAssigneeIdDto, QueryAssigneeId>(); // *** NEED THIS DIRECTION ***
+            CreateMap<IssueQueryDto, IssueQuery>(); // *** NEED THIS MAIN MAP ***
+
         }
     }
 }
