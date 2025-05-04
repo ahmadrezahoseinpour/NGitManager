a4c40d2230355946d8764c7ceb2e58ae0d0fec35
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-04 18:44:47 +0330

    Add Issue Enums

---
+++ GitManager/Dto/Issue/enums/IssueStateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitManager.Dto.Issue.enums
+{
+    public enum IssueStateDto
+    {
+        opened,
+        closed,
+    }
+}
---
+++ GitManager/Dto/Issue/enums/IssueTypeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitManager.Dto.Issue.enums
+{
+    public enum IssueTypeDto
+    {
+        issue,
+        incident,
+        test_case,
+    }
+}
