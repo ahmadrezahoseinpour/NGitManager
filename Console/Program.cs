f56c4fe8a951bfa4fc7cc25d32583415ddb630d7
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-04 18:44:30 +0330

    Update test project

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,7 +3,7 @@ using GitManager.Interface;
 using GitManager.Dto.Issue;
 using GitManager.Dto.Epic;
 using GitManager.Service.Utility;
-using GitManager.Dto.User;
+using GitManager.Dto.Issue.enums;
 
 class Program
 {
@@ -12,28 +12,53 @@ class Program
         // Configure services
         var services = new ServiceCollection();
         services.AddGitManager(
-            gitLabUrl: "https://git.daneshrefah.ir/",
-            personalAccessToken: "glpat-z1aYassa7QxGgPCdUJSo"
+
         );
         var serviceProvider = services.BuildServiceProvider();
 
         // Resolve service
         var gitService = serviceProvider.GetRequiredService<IGitManagerService>();
 
-        int groupId = 27;
-        // Example: Get Git URL for a project (replace with your project ID)
-        int projectId = 39;
-        // Example: Get an issue
+        //int groupId = 27;
+        //// Example: Get Git URL for a project (replace with your project ID)
+        //int projectId = 39;
+        //// Example: Get an issue
 
 
-        var test1 = await gitService.Issue.GetAll(299);
-        var issue = new IssueDto() { ProjectId = 299, Title = "first Issue" };
-        issue.Assignee = new();
-        issue.Assignee.Id = 35;
-        var test2 = await gitService.Issue.Close(299,11);
+        //var test1 = await gitService.Issue.GetAll(299);
+        //var issue = new IssueDto() { ProjectId = 299, Title = "first Issue" };
+        //issue.Assignee = new();
+        //issue.Assignee.Id = 35;
+
+        //var test3 = await gitService.Issue.Open(299, 1);
+        //if (test3) { Console.WriteLine("Opening was successful"); }
+
+        //IssueQueryDto qDtop = new()
+        //{
+        //    State = IssueStateDto.opened
+        //};
+        //var test1 = await gitService.Issue.Search(190, qDtop);
+        //var dto = new IssueDto()
+        //{
+
+        //};
+        //var res = gitService.Issue.Update(dto);
+        //var dto1 = new IssueDto()
+        //{
+        //    ProjectId = 299,
+        //    Title = "Second Issue",
+        //};
+        //var test = await gitService.Issue.Create(dto1);
+        //var test1 = await gitService.Issue.Get(299, 1);
+
+        var dto1 = new IssueDto()
+        {
+            ProjectId = 299,
+            IssueId = 2,
+        };
+        var dto2 = await gitService.User.GetByUserName("jorsar");
+        //Console.WriteLine($"this user is {dto.Name} with username : {dto.Username} and email : {dto.Email}");
 
 
-        if (test2) {
-            Console.WriteLine($"Issues 1 successfully opened."); }
     }
 }
