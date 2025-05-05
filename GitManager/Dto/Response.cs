eb54888158bca6d0dea1c35a5de0b3f1c7926274
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-05 16:41:51 +0330

    Add response dto

---
+++ GitManager/Dto/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitManager.Dto
+{
+    public class Response<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; } = "";
+        public T Data{ get; set; }
+    }
+}
